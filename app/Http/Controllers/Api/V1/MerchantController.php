5771ed5806d388fc27a9124fe0138ceb02180060
Bobobek Turdiyev <info@programmer.uz>
2023-12-20 01:13:23 +0500

    add: all merchants with filtering

--- app/Http/Controllers/Api/V1/MerchantController.php
+++ app/Http/Controllers/Api/V1/MerchantController.php
@@ -6,9 +6,34 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Merchant\StoreMerchantRequest;
 use App\Http\Requests\Api\V1\Merchant\UpdateMerchantRequest;
 use App\Models\Merchant;
+use Illuminate\Http\Request;
 
 class MerchantController extends Controller
 {
+
+
+    public function index(Request $request)
+    {
+        $query = Merchant::query();
+
+        // Example of applying filters
+        if ($request->has('name')) {
+            $query->where('name', 'like', '%' . $request->input('name') . '%');
+        }
+
+        if ($request->has('status')) {
+            $query->where('status', $request->input('status'));
+        }
+
+        if ($request->has('order_by_id')) {
+            $query->orderBy('id', $request->input('order_by_id'));
+        }
+
+        $merchants = $query->get();
+
+        return response()->json($merchants, 200);
+    }
+
 
     /**
      * Store a newly created resource in storage.
